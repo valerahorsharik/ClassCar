ffb762e4b828f316c52bce721183e5187faf6810
Valera Horsharik <valera.horsharik@gmail.com>
2016-03-11 15:15:50 +0200

    add const

--- passengerCar.php
+++ passengerCar.php
@@ -1,17 +1,20 @@
 <?php
 class PassengerCar extends Car
 {
+    const IN = "In car" ;
+    const OUT = "Out of car" ;
+
     private $passengers = array();
 		
     public function getIn(passenger $passenger)
     {
-        $passenger->state = "In car";
+        $passenger->state = IN;
         $this->passengers[] = $passenger;
     }
 		
     public function getOut(passenger $passenger)
     {
-        $passenger->state = "Out of car";
+        $passenger->state = OUT;
         foreach ($this->passengers as $out=>$value) {
             if ($value->getId() == $passenger->getId()) {
                 unset($this->passengers[$out]); 
