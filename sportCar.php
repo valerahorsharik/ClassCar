8d82e509e81d62d6510ff476f68720f951a399e4
Valera Horsharik <valera.horsharik@gmail.com>
2016-03-11 15:00:52 +0200

    Change free space

--- sportCar.php
+++ sportCar.php
@@ -7,14 +7,17 @@ class SportCar extends Car
     {
         return ($this->spoiler ? $this->max_speed * 1.1 : $this->max_speed) ;
     }
+		
     public function setSpoiler()
     {
         $this->spoiler = true;
     }
+		
     public function removeSpoiler()
     {
         $this->spoiler = false;
     }
+		
     public function getSpoiler()
     {
         return ($this->spoiler?"Spoiler installed":"Spoiler is not set");
